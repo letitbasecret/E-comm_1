b677a6b90e7a0ebc92e45e4d0e7cc70ccc659c36
priyanka <piru.29072k@gmail.com>
2024-06-02 22:35:38 +0530

    second- commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
-// import './App.css'
-import { Button } from "react-bootstrap";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Header";
+import AddProduct from "./AddProduct";
+import Update from "./Update";
+import Register from "./Register";
+import Login from "./Login";
+import "./App.css";
+
 function App() {
   return (
-    <>
-      <p>E-commm</p>
-      <Button>click me</Button>
-    </>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/add" element={<AddProduct />} />
+          <Route path="/update" element={<Update />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ frontend/src/Header.jsx
@@ -0,0 +1,21 @@
+// import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
+
+function Header() {
+  return (
+    <div>
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto nav-bar">
+          <Link to="/add">AddProduct</Link>
+          <Link to="/update">Update</Link>
+          <Link to="/register">Register</Link>
+          <Link to="/login">Login</Link>
+        </Nav>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Header;
