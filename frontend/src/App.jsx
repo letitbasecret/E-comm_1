d32632a17a7846870a7c2e37f43edd9d3b730288
priyanka <piru.29072k@gmail.com>
2024-06-02 17:01:08 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+// import './App.css'
+import { Button } from "react-bootstrap";
+function App() {
+  return (
+    <>
+      <p>E-commm</p>
+      <Button>click me</Button>
+    </>
+  );
+}
+
+export default App;
